7464d8158dc11e623f569ffd3de00a0c59a3eec3
David Garson <garson.david@gmail.com>
2022-06-29 14:17:25 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project seeks to identify narrative markers in music lyrics',
+    author='DavidGarson',
+    license='MIT',
+)
