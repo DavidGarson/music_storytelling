00f411e3fbe3b4464c609c4348d7ed21fac8bcb1
David Garson <garson.david@gmail.com>
2022-06-30 00:10:02 -0400

    RIAA raw dataset

---
+++ src/data/create_riaa_raw_dataset.py
@@ -0,0 +1,27 @@
+import os
+import argparse
+import pandas as pd
+from utils import read_params
+
+def create_riaa_raw_dataset(config_path):
+    config = read_params(config_path = config_path)
+    dir_name = config['data_source']['RIAA']
+    df_list = []
+    df_riaa = pd.DataFrame()
+    
+    for file in os.listdir(dir_name):
+        if file.endswith('csv'):
+            df_path = os.path.join(dir_name, file)
+            df = pd.read_csv(df_path, sep = ';', encoding='latin-1')
+            df_list.append(df)
+    csv_merged = pd.concat(df_list, ignore_index=True)
+    new_cols = [col.replace(" ","_") for col in csv_merged.columns]
+    raw_riaa_path = config['load_data']['raw_riaa_csv']
+    csv_merged.to_csv(raw_riaa_path,sep=';', index=False,header=new_cols)
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    default_config_path = os.path.join("config", "params.yaml")
+    args.add_argument("--config", default=default_config_path)
+    parsed_args = args.parse_args()
+    create_riaa_raw_dataset(config_path=parsed_args.config)
--- src/data/get_billboard_hot_100.py
+++ src/data/get_billboard_hot_100.py
@@ -1,25 +1,12 @@
 import os
-import yaml
-import pandas as pd
 import argparse
 import kaggle
-
-
-
-
-
-
-def read_params(config_path):
-    with open(config_path) as yaml_file:
-        config = yaml.safe_load(yaml_file)
-    return config
-
+from utils import read_params
 
 def get_billboard_hot_100(config_path):
     config = read_params(config_path)
     kaggle_dataset = config['kaggle']['dataset']
     save_path = config['data_source']['Billboard']
-    print(kaggle_dataset)
     kaggle.api.authenticate()
     kaggle.api.dataset_download_files(kaggle_dataset, path=save_path, unzip=True)
 
